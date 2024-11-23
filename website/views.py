7079ee9f18b48571475aec5a67bfdac6211f5f29
murtazo <toshniyozovmurtazo.r538@gmail.com>
2024-11-24 02:29:29 +0900

    Login and logout logic implementation

--- website/views.py
+++ website/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
 
 
 # Create your views here.
-
+@csrf_protect
 def home(request):
-    return render(request, 'home.html', {})
+
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        user_password = request.POST['password']
+
+        #Authenticate user
+        user = authenticate(request, username=user_name, password=user_password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return render(request, 'home.html')
+    else:
+        return render(request, 'home.html', {})
 
 def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You have been logged out successfully')
+    return redirect('home')
 
