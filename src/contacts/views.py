750c6bf24744e9b841ed7b72b2238a76ff86fe4b
iamurtazo <murtazo207@inha.edu>
2025-11-07 12:28:05 +0900

    Add contacts app with URL routing, views, and template for displaying contact list

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import Contact
+
+
+@login_required
+def contacts(request, *args, **kwargs):
+    # user = request.user
+    qs = Contact.objects.all()
+
+    context = {"object_list": qs}
+
+    return render(request, "contacts/contacts_list.html", context)
